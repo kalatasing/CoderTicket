6b34c3f7caa39c85a9c55a76cb01bb8cc19f1a14
Luan Ta <luan@tinypulse.com>
2017-07-02 17:35:04 +0700

    Done until Buy ticket - broken layout

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,15 @@
 class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  belongs_to :ticket_type
+
+  def self.ticket_validate(event, ticket_type_id, number_of_order)
+    tickets = Ticket.where(event_id: event.id)
+                    .where(ticket_type_id: ticket_type_id)
+
+    max_quantity = event.ticket_types.find(ticket_type_id).max_quantity
+
+    available = max_quantity - tickets.count
+    return (number_of_order <= available)
+  end
 end
